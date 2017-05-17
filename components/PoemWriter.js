7fc62dc65d57907e0fd57838ee743862dbec9e30
christopher-hague <christopher.a.hague@gmail.com>
2017-05-17 10:43:31 -0400

    done

--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -4,14 +4,42 @@ export default class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poemTextArea: "",
+      validity: false
+    };
+
+    this.handlePoemText = this.handlePoemText.bind(this)
+  }
+
+  handlePoemText(event) {
+    const poemText = event.target.value
+    this.setState({
+      poemTextArea: poemText
+    })
+
+
+    this.setState({
+      validity: false
+    })
+
+    const poemLines = poemText.split("\n")
+    if(poemLines.length === 3) {
+      if(poemLines[0].split(" ").filter( word => word).length === 5 && poemLines[1].split(" ").filter( word => word).length === 3 && poemLines[2].split(" ").filter( word => word).length === 5) {
+        this.setState({
+          validity: true
+        })
+      }
+    }
   }
 
   render() {
+    // uses poemDiv in ternary when poem is valid
+    const poemDiv = <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" value={this.state.poemTextArea} onChange={this.handlePoemText}/>
+        {this.state.validity ? "" : poemDiv}
       </div>
     );
   }
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -4,14 +4,25 @@ export default class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.handleMessage = this.handleMessage.bind(this)
+
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleMessage(event) {
+    this.setState({
+      value: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleMessage} />
+        <p>Characters Remaining: {this.props.maxChars - this.state.value.length}</p>
       </div>
     );
   }
